2a2d484155ec07c4286b2e8d2b5bb3d24873c479
kasravi1 <kasravi1@jrlogin10.jureca>
2026-07-28 16:31:05 +0200

    add slurm file for SANTIS

--- submission_file_santis.sh
+++ submission_file_santis.sh
@@ -6,11 +6,15 @@
 #SBATCH --partition=normal
 #SBATCH --account=ch17
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4
 #SBATCH --time=00:05:00
 
-uenv run prgenv-gnu/25.6:v2 --view=modules,default -- \
-    srun --export=ALL --ntasks=1 --gpus-per-task=4 \
-    source .venv/bin/activate /
-    python flash_attn_experiment.py
+MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
+# Allow communication over InfiniBand cells.
+export MASTER_ADDR="${MASTER_ADDR}"
+export MASTER_PORT="${MASTER_PORT:-29500}"
+
+source /p/project1/cjsc/kasravi1/WeatherGenerator/.venv/bin/activate
+
+srun --export=ALL python flash_attn_experiment_geo_parallelization.py
